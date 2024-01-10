c59fca4834ff2ee42b681fd0cdb1bb95c297d273
GreenWizard2015 <forwork.anton@gmail.com>
2024-01-10 22:09:29 +0000

    rework notifications

--- ui/src/store/slices/SystemStatus.js
+++ ui/src/store/slices/SystemStatus.js
@@ -1,20 +1,49 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { NotificationsSystemActions } from './Notifications';
+
+function withNotification(action, message) {
+  return async (params, { dispatch }) => {
+    try {
+      return await action(params);
+    } catch(error) {
+      dispatch(NotificationsSystemActions.alert({
+        type: 'error',
+        message: `${message} (${error.message})`
+      }));
+      throw error;
+    }
+  };
+}
 
 // Async thunks
 export const startPump = createAsyncThunk(
   'systemStatus/startPump',
-  async ({ api, pouringTime }, { dispatch }) => {
-    const response = await api.start(pouringTime);
-    return response;
-  }
+  withNotification(
+    async ({ api, pouringTime }) => {
+      return await api.start(pouringTime);
+    },
+    'Failed to start pump'
+  )
 );
 
 export const stopPump = createAsyncThunk(
   'systemStatus/stopPump',
-  async ({ api }, { dispatch }) => {
-    const response = await api.stop();
-    return response;
-  }
+  withNotification(
+    async ({ api }) => {
+      return await api.stop();
+    },
+    'Failed to stop pump'
+  )
+);
+
+export const updateSystemStatus = createAsyncThunk(
+  'systemStatus/update',
+  withNotification(
+    async ( api ) => {
+      return await api.status();
+    },
+    'Failed to update system status'
+  )
 );
 
 // slice for system status
@@ -25,18 +54,17 @@ const bindStatus = (state, action) => {
 export const SystemStatusSlice = createSlice({
   name: 'systemStatus',
   initialState: null,
-  reducers: {
-    updateSystemStatus: bindStatus,
-  },
+  reducers: {},
   extraReducers: (builder) => {
     // update system status on start/stop pump
     builder.addCase(startPump.fulfilled, bindStatus);
     builder.addCase(stopPump.fulfilled, bindStatus);
+    builder.addCase(updateSystemStatus.fulfilled, bindStatus);
     // on error, do not update system status
     builder.addCase(startPump.rejected, (state, action) => state);
     builder.addCase(stopPump.rejected, (state, action) => state);
+    builder.addCase(updateSystemStatus.rejected, (state, action) => state);
   }
 });
 
 export const actions = SystemStatusSlice.actions;
-export const { updateSystemStatus } = actions;
--- ui/src/store/slices/index.js
+++ ui/src/store/slices/index.js
@@ -1,7 +1,8 @@
 import { SystemStatusSlice } from "./SystemStatus";
 import { UISlice } from "./UI";
+import { NotificationsSlice } from "./Notifications";
 
-const slices = [ SystemStatusSlice, UISlice ];
+const slices = [ SystemStatusSlice, UISlice, NotificationsSlice ];
 // export all slices as an object { [sliceName]: slice }
 export const ALL_APP_SLICES = slices.reduce((acc, slice) => {
   acc[slice.name] = slice;
